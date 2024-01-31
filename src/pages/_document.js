537e4e897242c7d60f9328b90cfb90a080b81910
mani99brar <mani99brar@gmail.com>
2024-01-31 22:30:38 +0530

    feat: tab

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
-
+import { Html, Head, Main, NextScript } from "next/document";
+import Image from "next/image";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="netzerologo.png"
+        />
+        <title>Net Zero Carbon Homes Pilot Project, Mohali, INDIA</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
